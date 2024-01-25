606f3a31980dce1ee235b994fffae3c0a98c270c
Odmund Bautista <odfelip.bautista@gmail.com>
2024-01-25 16:50:27 -0600

    Refactor code
    Continue with Step 7 tomorrow

--- script.js
+++ script.js
@@ -13,85 +13,95 @@ const divisionSymbol = document.querySelector('.divide');
 const subtractionSymbol = document.querySelector('.subtract');
 const additionSymbol = document.querySelector('.add');
 const equalsSign = document.querySelector('.equals');
+const clearSign = document.querySelector('.clear');
 
 // Variables
-let firstDigit;
-let secondDigit;
-let operationSymbol;
+let operand1 = 0;
+let operand2 = 0;
+let operatorSymbol;
 let displayValue = display.textContent;
 
-function updateDisplay(){
-    for (let i=0; i < numbers.length; i++) {
-        numbers[i].addEventListener('click', () => {
-            if (display.textContent === '0') {
-                display.textContent = '';
-                display.textContent += numbers[i].textContent;
-            } else {
-                display.textContent += numbers[i].textContent;
-            }
-            displayValue = display.textContent;
-        });
+function handleNumberInput(number) {
+    if (display.textContent === '0') {
+        display.textContent = '';
     }
-    
-    for (let i=0; i < operators.length; i++) {
-        operators[i].addEventListener('click', () => {
-            if (!firstDigit) {
-                firstDigit = display.textContent;
-                operationSymbol = operators[i].textContent;
-            } else if (!secondDigit) {
-                secondDigit = display.textContent;
-            }
-            display.textContent = 0;
-            displayValue = display.textContent;
-        });
-    }
+    display.textContent += number;
+    displayValue = display.textContent;
 }
 
-equalsSign.addEventListener('click', () => {
-    if (!secondDigit) {
-        secondDigit = display.textContent;
+function handleOperatorInput(operator) {
+    if (!operand1) {
+        operand1 = display.textContent;
+        operatorSymbol = operator.textContent;
+    } else if (!operand2) {
+        operand2 = display.textContent;
     }
-    display.textContent = (operate(firstDigit, operationSymbol, secondDigit)).toString();
-});
-
-// function checkOperator(operator){
-//     if (operator.textContent === '+') {
-//         operate(firstDigit, '+', secondDigit);
-//     } else if (operator.textContent === '-') {
-//         operate(firstDigit, '-', secondDigit);
-//     } else if (operator.textContent === '/') {
-//         operate(firstDigit, '/', secondDigit);
-//     } else if (operator.textContent === 'x') {
-//         operate(firstDigit, '*', secondDigit);
-//     }
-// }
-
-function add(a,b) {
-    return a + b;
+    display.textContent = 0;
+    displayValue = display.textContent;
 }
 
-function subtract(a,b) {
+function showCalculation() {
+    if (!operand2) {
+        operand2 = display.textContent;
+    }
+    display.textContent = operate(operand1, operatorSymbol, operand2).toString();
+}
+
+function resetCalculator(){
+    operand1 = null;
+    operand2 = null;
+    operatorSymbol = null;
+    displayValue = null;
+    display.textContent = 0;
+}
+
+function add(a, b) {
+    return +a + +b;
+}
+
+function subtract(a, b) {
     return a - b;
 }
 
-function multiply(a,b) {
-    return a*b;
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide(a,b) {
+function divide(a, b) {
+    if (b === 0) {
+        return 'Error: Division by zero';
+    }
     return a / b;
 }
 
 function operate(a, operator, b) {
     if (operator === '+') {
-        return add(a,b);
+        return add(a, b);
     } else if (operator === '-') {
-        return subtract(a,b);
+        return subtract(a, b);
     } else if (operator === 'x') {
-        return multiply(a,b);
+        return multiply(a, b);
     } else {
-        return divide(a,b);
+        return divide(a, b);
     }
 }
 
-updateDisplay();
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', () => {
+        handleNumberInput(numbers[i].textContent);
+    });
+}
+
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener('click', () => {
+        handleOperatorInput(operators[i]);
+    });
+}
+
+clearSign.addEventListener('click', () => {
+    resetCalculator();
+});
+
+equalsSign.addEventListener('click', () => {
+    showCalculation();
+});
